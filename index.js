bd5a999e295926443d797cbb91eb022053a9ed3a
David Bright Ogagaoghene <brightdavid951@gmail.com>
2023-11-30 11:24:45 +0100

    Edited Copy Hex code

--- index.js
+++ index.js
@@ -36,18 +36,30 @@ function ResetBtn() {
 }
 
 function CopyHEX() {
-    
-    const copyHexCode = color_Box;
-    const Input = document.createElement('input');
-    document.body.appendChild(Input);
-    Input.value = copyHexCode;
-    Input.seect();
+    const colorText = color_Box.style.backgroundColor;
+  const hexCode = rgbToHex(colorText);
+    const tempInput = document.createElement('input');
+    document.body.appendChild(tempInput);
+    tempInput.value = hexCode;
+    tempInput.select();
     document.execCommand('copy');
-    document.body.removeChild(Input);
-
-    alert(`Color copied to clipboard ${copyHexCode}`)
+    document.body.removeChild(tempInput);
+    alert(`Color copied to clipboard: ${hexCode}`)
+   
 }
+ function rgbToHex(rgbColor) {
+     const regex = /rgb\((\d+), (\d+), (\d+)\)/;
+     const matches = regex.exec(rgbColor);
+     const r = parseInt(matches[1]);
+     const g = parseInt(matches[2]);
+     const b = parseInt(matches[3]);
+     return "#" + componentToHex(r) + componentToHex(g) +componentToHex(b);
+ }
 
+ function componentToHex(c) {
+     const hex = c.toString(16);
+     return hex.length == 1 ? "0" + hex : hex;
+ }
 function RGB() {
     const colorText = color_Box.style.backgroundColor;
     const tempInput = document.createElement('input');
@@ -56,5 +68,5 @@ function RGB() {
     tempInput.select();
     document.execCommand('copy')
     document.body.removeChild(tempInput);
-    alert(`Color copied to clipboard ${colorText}`)
+    alert(`Color copied to clipboard: ${colorText}`)
 }
