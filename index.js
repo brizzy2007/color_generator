063e900b4d4712f16e1ca977681349f87aac9b37
David Bright Ogagaoghene <brightdavid951@gmail.com>
2023-11-30 09:54:06 +0100

    Color Generator

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const color_Box = document.getElementById('color-box');
 const colorCode = document.getElementById('colorCode'); 
 
 
-let color_code = '';
+
 
 
 function GenerateBtn() {
@@ -30,17 +30,38 @@ function GenerateBtn() {
 }
 
 function ResetBtn() {
-    const hexColor = '';
-    color_Box.style.backgroundColor = hexColor;
+    // const hexColor = '';
+    color_Box.style.backgroundColor = '';
     colorCode.innerHTML = '';
 }
 
 function CopyHEX() {
-    let hexColor = '#';
-    for (let i = 0; i < 6; i++) {
-        hexColor += hexChars[Math.floor(Math.random() * 16)] 
-        
-    };
+    // Get the text content of the colorCode element
+    let copyHexCode = colorCode.innerHTML;
+
+
+    // Create a temporary input element
+    const input = document.createElement('input');
+    input.value = copyHexCode;
+
+
+    // Append the input element to the body
+    document.body.appendChild(input);
+
+
+    // Select the text in the input element
+    input.select();
+
+
+     // Copy the selected text to the clipboard
+    document.execCommand('copy');
+
+
+    
+    // Remove the temporary input element
+    document.body.removeChild(input);
+
+    alert('Hex code copied to clipboard' + copyHexCode)
 }
 
 function RGB() {
